c8db74786d75b85a4a6fb376fa01cc3535ca049b
feng <1034013213@qq.com>
2024-08-23 19:37:48 +0800

    mr

--- components/MdNavbar.tsx
+++ components/MdNavbar.tsx
@@ -2,19 +2,19 @@
 import React from 'react'
 import Logo from './Logo';
 import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from '@nextui-org/react';
-import { Activity, ChevronDown, Flash, Lock, Scale, Server, TagUser } from './Icons';
+// import { Activity, ChevronDown, Flash, Lock, Scale, Server, TagUser } from './Icons';
 
 export default function MdNavbar() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  const icons = {
-    chevron: <ChevronDown fill="currentColor" size={16} />,
-    scale: <Scale className="text-warning" fill="currentColor" size={30} />,
-    lock: <Lock className="text-success" fill="currentColor" size={30} />,
-    activity: <Activity className="text-secondary" fill="currentColor" size={30} />,
-    flash: <Flash className="text-primary" fill="currentColor" size={30} />,
-    server: <Server className="text-success" fill="currentColor" size={30} />,
-    user: <TagUser className="text-danger" fill="currentColor" size={30} />,
-  };
+  // const icons = {
+  //   chevron: <ChevronDown fill="currentColor" size={16} />,
+  //   scale: <Scale className="text-warning" fill="currentColor" size={30} />,
+  //   lock: <Lock className="text-success" fill="currentColor" size={30} />,
+  //   activity: <Activity className="text-secondary" fill="currentColor" size={30} />,
+  //   flash: <Flash className="text-primary" fill="currentColor" size={30} />,
+  //   server: <Server className="text-success" fill="currentColor" size={30} />,
+  //   user: <TagUser className="text-danger" fill="currentColor" size={30} />,
+  // };
   const menuItems = [
     "Profile",
     "Dashboard",
@@ -53,7 +53,7 @@ export default function MdNavbar() {
               <Button
                 disableRipple
                 className="p-0 bg-transparent data-[hover=true]:bg-transparent"
-                endContent={icons.chevron}
+                // endContent={icons.chevron}
                 radius="sm"
                 variant="light"
               >
@@ -71,35 +71,35 @@ export default function MdNavbar() {
             <DropdownItem
               key="autoscaling"
               description="ACME scales apps to meet user demand, automagically, based on load."
-              startContent={icons.scale}
+              // startContent={icons.scale}
             >
               Autoscaling
             </DropdownItem>
             <DropdownItem
               key="usage_metrics"
               description="Real-time metrics to debug issues. Slow query added? We’ll show you exactly where."
-              startContent={icons.activity}
+              // startContent={icons.activity}
             >
               Usage Metrics
             </DropdownItem>
             <DropdownItem
               key="production_ready"
               description="ACME runs on ACME, join us and others serving requests at web scale."
-              startContent={icons.flash}
+              // startContent={icons.flash}
             >
               Production Ready
             </DropdownItem>
             <DropdownItem
               key="99_uptime"
               description="Applications stay on the grid with high availability and high uptime guarantees."
-              startContent={icons.server}
+              // startContent={icons.server}
             >
               +99% Uptime
             </DropdownItem>
             <DropdownItem
               key="supreme_support"
               description="Overcome any challenge with a supporting team ready to respond."
-              startContent={icons.user}
+              // startContent={icons.user}
             >
               +Supreme Support
             </DropdownItem>
